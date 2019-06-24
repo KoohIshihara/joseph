ceaadcbdf5e653a59b31289cfc5187ba6e150bfa
KouIshihara <ko.ishihara@chatcenter.io>
2019-06-24 16:37:59 +0900

    init

---
+++ src/views/SignIn.vue
@@ -0,0 +1,21 @@
+<template>
+  <SignIn :redirect="redirect"/>
+</template>
+
+<script>
+import SignIn from '@/components/sign-in/firebase-sign-in-ui'
+export default {
+  components: {
+    SignIn
+  },
+  props: {
+    redirect: {
+      type: String,
+      default: '/'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
