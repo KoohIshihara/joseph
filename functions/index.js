dae133a12ebb222b3c7c936673cd7a843d238f1e
KouIshihara <ko.ishihara@chatcenter.io>
2019-07-02 15:47:30 +0900

    OGP

--- functions/index.js
+++ functions/index.js
@@ -47,3 +47,83 @@ exports.pushGroupIdToUsers = functions.https.onRequest(async (req, res) => {
     res.status(200).end(JSON.stringify({ result: 'OK' }))
   }) // cors
 })
+
+// chat/* のOGP
+const appDomain = 'https://joseph-alike.firebaseapp.com'
+// const appDomain = 'http://localhost:5000'
+const OGP_IMG_WIDTH = 1200
+const OGP_IMG_HEIGHT = 630
+const OGP_SRC = 'https://firebasestorage.googleapis.com/v0/b/joseph-alike.appspot.com/o/joseph_ogp.png?alt=media&token=70a8b3e4-3eea-45bc-b59b-f74812afb17c'
+
+exports.chatRedirect = functions.https.onRequest(async (req, res) => {
+  cors(req, res, async () => {
+    const groupId = req.path.split('/')[2]
+
+    const group = await db
+      .collection('GROUP')
+      .doc(groupId)
+      .get()
+      .then(d => {
+        return d.data()
+      })
+
+    // console.log('group id:\n', group)
+
+    const persons = await db
+      .collection('PERSON')
+      .where('groups', 'array-contains', groupId)
+      .get()
+      .then((q) => {
+        return q.docs.map((d) => {
+          return d.data()
+        })
+      })
+
+    // console.log('persons:\n', persons)
+
+    const groupName = group.name
+    var groupSubtitle = `${persons[0].name}, `
+    for (var i = 1; i < persons.length; i++) {
+      groupSubtitle += `${persons[i].name}, `
+    }
+
+    console.log('groupName', groupName)
+    console.log('groupSubtitle', groupSubtitle)
+
+    const html = createHtml(groupName, groupSubtitle, groupId)
+    res.set('Cache-Control', 'public, max-age=600, s-maxage=600')
+    res.status(200).end(html)
+  }) // cors
+})
+
+const createHtml = (groupName, groupSubtitle, groupId) => {
+  const SITEURL = appDomain
+  const PAGEURL = `${SITEURL}/@chat/${groupId}`
+  const TITLE = groupName
+  const DESCRIPTION = groupSubtitle
+  return `<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>colorinco</title>
+    <meta property="og:title" content="${TITLE}">
+    <meta property="og:image" content="${OGP_SRC}">
+    <meta property="og:image:width" content="${OGP_IMG_WIDTH}">
+    <meta property="og:image:height" content="${OGP_IMG_HEIGHT}">
+    <meta property="og:description" content="${DESCRIPTION}">
+    <meta property="og:url" content="${PAGEURL}">
+    <meta property="og:type" content="article">
+    <meta property="og:site_name" content="${TITLE}">
+    <meta name="twitter:site" content="${SITEURL}">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="${TITLE}">
+    <meta name="twitter:image" content="${OGP_SRC}">
+    <meta name="twitter:description" content="${DESCRIPTION}">
+  </head>
+  <body>
+    <script type="text/javascript">window.location="/@chat/${groupId}";</script>
+  </body>
+</html>
+`
+}
